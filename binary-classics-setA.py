7a6a9ec2af89621a8346b937d8f7d238e564cbd9
KShivendu <shivendu@iitbhilai.ac.in>
2021-03-09 22:40:45 +0530

    Cleanup

--- binary-classics-setA.py
+++ binary-classics-setA.py
@@ -37,108 +37,108 @@ parameters = [{
 nested_cross_val(pipe, parameters, X, y, "MultiNB",
                  n_jobs=18, filename='setA.txt')
 
-# bNB = BernoulliNB(binarize=0.5)
-# vec = TfidfVectorizer(analyzer='word')
-# pipe = Pipeline(steps=[('vec', vec), ('bNB', bNB)])
-# parameters = [{
-#     'vec__ngram_range': [(1, 1), (1, 2), (1, 5)],
-#     'vec__max_features':[5000, 10000, 50000, 100000],
-#     'vec__stop_words':['english', None],
-#     'bNB__alpha':[1, 0.1, 0.01, 0.001, 0.0001, 0.00001]
-# }]
-# nested_cross_val(pipe, parameters, X, y, "BernouNB")
+bNB = BernoulliNB(binarize=0.5)
+vec = TfidfVectorizer(analyzer='word')
+pipe = Pipeline(steps=[('vec', vec), ('bNB', bNB)])
+parameters = [{
+    'vec__ngram_range': [(1, 1), (1, 2), (1, 5)],
+    'vec__max_features':[5000, 10000, 50000, 100000],
+    'vec__stop_words':['english', None],
+    'bNB__alpha':[1, 0.1, 0.01, 0.001, 0.0001, 0.00001]
+}]
+nested_cross_val(pipe, parameters, X, y, "BernouNB")
 
-# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-#                     Run Logistic Regression
-# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-# log = LogisticRegression(random_state=0, max_iter=1000, n_jobs=-1)
-# vec = TfidfVectorizer(analyzer='word')
-# pipe = Pipeline(steps=[('vec', vec), ('log', log)])
-# parameters = [{
-#     'vec__ngram_range': [(1, 1), (1, 2), (1, 5)],
-#     'vec__max_features':[5000, 10000, 50000, 100000],
-#     'vec__stop_words':['english', None],
-#     'log__C':[0.5, 1, 3, 5, 10, 1000],
-#     'log__solver':['newton-cg', 'lbfgs', 'sag'],
-#     'log__penalty':['l2']
-# }, {
-#     'vec__ngram_range': [(1, 1), (1, 2), (1, 5)],
-#     'vec__max_features':[5000, 10000, 50000, 100000],
-#     'vec__stop_words':['english', None],
-#     'log__C':[0.5, 1, 3, 5, 10, 1000],
-#     'log__solver':['saga'],
-#     'log__penalty':['l1']
-# }]
-# nested_cross_val(pipe, parameters, X, y, "LogReg")
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+                    Run Logistic Regression
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+log = LogisticRegression(random_state=0, max_iter=1000, n_jobs=-1)
+vec = TfidfVectorizer(analyzer='word')
+pipe = Pipeline(steps=[('vec', vec), ('log', log)])
+parameters = [{
+    'vec__ngram_range': [(1, 1), (1, 2), (1, 5)],
+    'vec__max_features':[5000, 10000, 50000, 100000],
+    'vec__stop_words':['english', None],
+    'log__C':[0.5, 1, 3, 5, 10, 1000],
+    'log__solver':['newton-cg', 'lbfgs', 'sag'],
+    'log__penalty':['l2']
+}, {
+    'vec__ngram_range': [(1, 1), (1, 2), (1, 5)],
+    'vec__max_features':[5000, 10000, 50000, 100000],
+    'vec__stop_words':['english', None],
+    'log__C':[0.5, 1, 3, 5, 10, 1000],
+    'log__solver':['saga'],
+    'log__penalty':['l1']
+}]
+nested_cross_val(pipe, parameters, X, y, "LogReg")
 
-# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-#                             Run SVM
-# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-# svm = SVC(random_state=0)
-# vec = TfidfVectorizer(analyzer='word')
-# pipe = Pipeline(steps=[('vec', vec), ('svm', svm)])
-# parameters = [{
-#     'vec__ngram_range': [(1, 1), (1, 2), (1, 5)],
-#     'vec__max_features':[5000, 10000, 50000, 100000],
-#     'vec__stop_words':['english', None],
-#     'svm__kernel':['rbf'],
-#     'svm__C':[0.25, 0.5, 1, 3, 5, 10, 100, 1000],
-#     'svm__gamma':[0.05, 0.1, 0.5, 0.9, 1]
-# }, {
-#     'vec__ngram_range': [(1, 1), (1, 2), (1, 5)],
-#     'vec__max_features':[5000, 10000, 50000, 100000],
-#     'vec__stop_words':['english', None],
-#     'svm__kernel':['linear'],
-#     'svm__C':[0.25, 0.5, 1, 3, 5, 10, 100, 1000]
-# }]
-# nested_cross_val(pipe, parameters, X, y, "SVM")
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+                            Run SVM
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+svm = SVC(random_state=0)
+vec = TfidfVectorizer(analyzer='word')
+pipe = Pipeline(steps=[('vec', vec), ('svm', svm)])
+parameters = [{
+    'vec__ngram_range': [(1, 1), (1, 2), (1, 5)],
+    'vec__max_features':[5000, 10000, 50000, 100000],
+    'vec__stop_words':['english', None],
+    'svm__kernel':['rbf'],
+    'svm__C':[0.25, 0.5, 1, 3, 5, 10, 100, 1000],
+    'svm__gamma':[0.05, 0.1, 0.5, 0.9, 1]
+}, {
+    'vec__ngram_range': [(1, 1), (1, 2), (1, 5)],
+    'vec__max_features':[5000, 10000, 50000, 100000],
+    'vec__stop_words':['english', None],
+    'svm__kernel':['linear'],
+    'svm__C':[0.25, 0.5, 1, 3, 5, 10, 100, 1000]
+}]
+nested_cross_val(pipe, parameters, X, y, "SVM")
 
-# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-#                             Run RidgeClassifier
-# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-# ridge = RidgeClassifier(random_state=0, fit_intercept=False)
-# vec = TfidfVectorizer(analyzer='word')
-# pipe = Pipeline(steps=[('vec', vec), ('ridge', ridge)])
-# parameters = [{
-#     'vec__ngram_range': [(1, 1), (1, 2), (1, 5)],
-#     'vec__max_features':[5000, 10000, 50000, 100000],
-#     'vec__stop_words':['english', None],
-#     'ridge__solver':['cholesky', 'lsqr', 'sparse_cg', 'saga'],
-#     'ridge__alpha':[1, 0.1, 0.01, 0.001, 0.0001, 0]
-# }]
-# nested_cross_val(pipe, parameters, X, y, "Ridge")
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+                            Run RidgeClassifier
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+ridge = RidgeClassifier(random_state=0, fit_intercept=False)
+vec = TfidfVectorizer(analyzer='word')
+pipe = Pipeline(steps=[('vec', vec), ('ridge', ridge)])
+parameters = [{
+    'vec__ngram_range': [(1, 1), (1, 2), (1, 5)],
+    'vec__max_features':[5000, 10000, 50000, 100000],
+    'vec__stop_words':['english', None],
+    'ridge__solver':['cholesky', 'lsqr', 'sparse_cg', 'saga'],
+    'ridge__alpha':[1, 0.1, 0.01, 0.001, 0.0001, 0]
+}]
+nested_cross_val(pipe, parameters, X, y, "Ridge")
 
-# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-#                             Run DecisionTree
-# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-# dTree = DecisionTreeClassifier(random_state=0)
-# vec = TfidfVectorizer(analyzer='word')
-# pipe = Pipeline(steps=[('vec', vec), ('dTree', dTree)])
-# parameters = [{
-#     'vec__ngram_range': [(1, 1), (1, 2), (1, 5)],
-#     'vec__max_features':[5000, 10000, 50000, 100000],
-#     'vec__stop_words':['english', None],
-#     'dTree__criterion':['gini', 'entropy'],
-#     'dTree__max_depth':[1, 2, 3, 4, 5, 10, 25, 50, 100, 200],
-#     'dTree__max_features':[2, 3, 4, 5, 'sqrt', 'log2', None],
-#     'dTree__min_samples_leaf': [1, 2, 3, 4, 5],
-#     'dTree__min_samples_split': [2, 4, 8, 10, 12]
-# }]
-# nested_cross_val(pipe, parameters, X, y, "DTree")
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+                            Run DecisionTree
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+dTree = DecisionTreeClassifier(random_state=0)
+vec = TfidfVectorizer(analyzer='word')
+pipe = Pipeline(steps=[('vec', vec), ('dTree', dTree)])
+parameters = [{
+    'vec__ngram_range': [(1, 1), (1, 2), (1, 5)],
+    'vec__max_features':[5000, 10000, 50000, 100000],
+    'vec__stop_words':['english', None],
+    'dTree__criterion':['gini', 'entropy'],
+    'dTree__max_depth':[1, 2, 3, 4, 5, 10, 25, 50, 100, 200],
+    'dTree__max_features':[2, 3, 4, 5, 'sqrt', 'log2', None],
+    'dTree__min_samples_leaf': [1, 2, 3, 4, 5],
+    'dTree__min_samples_split': [2, 4, 8, 10, 12]
+}]
+nested_cross_val(pipe, parameters, X, y, "DTree")
 
-# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-#                             Run RandomForest
-# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-# randFor = RandomForestClassifier(random_state=0, n_jobs=-1)
-# vec = TfidfVectorizer(analyzer='word')
-# pipe = Pipeline(steps=[('vec', vec), ('randFor', randFor)])
-# parameters = [{
-#     'vec__ngram_range': [(1, 1), (1, 2), (1, 5)],
-#     'vec__max_features':[5000, 10000, 50000, 100000],
-#     'vec__stop_words':['english', None],
-#     'randFor__max_depth':[1, 10, 50, 100, 200],
-#     'randFor__max_features':['sqrt', 'log2', None],
-#     'randFor__bootstrap':[True, False],
-#     'randFor__n_estimators': [10, 100, 500, 1000]
-# }]
-# nested_cross_val(pipe, parameters, X, y, "RandomForest")
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+                            Run RandomForest
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+randFor = RandomForestClassifier(random_state=0, n_jobs=-1)
+vec = TfidfVectorizer(analyzer='word')
+pipe = Pipeline(steps=[('vec', vec), ('randFor', randFor)])
+parameters = [{
+    'vec__ngram_range': [(1, 1), (1, 2), (1, 5)],
+    'vec__max_features':[5000, 10000, 50000, 100000],
+    'vec__stop_words':['english', None],
+    'randFor__max_depth':[1, 10, 50, 100, 200],
+    'randFor__max_features':['sqrt', 'log2', None],
+    'randFor__bootstrap':[True, False],
+    'randFor__n_estimators': [10, 100, 500, 1000]
+}]
+nested_cross_val(pipe, parameters, X, y, "RandomForest")
